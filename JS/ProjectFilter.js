57025e8687d0732f14853d4b0baf06b01c0b10b4
DubChen <wchen1642@gmail.com>
2016-09-27 12:57:05 -0700

    Isotope debugging

--- JS/ProjectFilter.js
+++ JS/ProjectFilter.js
@@ -4,6 +4,7 @@ var $grid = $('.grid').imagesLoaded( function() {
   // init Isotope after all images have loaded
   $grid.isotope({
     itemSelector: '.element-item',
+    layoutMode: 'fitRows',
     percentPosition:true
   });
 });
