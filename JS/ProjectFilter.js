29e23946f1cc3e6764c3db1b15618453f99fe007
DubChen <wchen1642@gmail.com>
2016-09-26 23:14:27 -0700

    Debugging
    Removed extraneous function call from ProjectFilter.js and changed pic
    names in index.html to facilitate debugging

--- JS/ProjectFilter.js
+++ JS/ProjectFilter.js
@@ -15,8 +15,6 @@ filtersElem.addEventListener( 'click', function( event ) {
     return;
   }
   var filterValue = event.target.getAttribute('data-filter');
-  // use matching filter function
-  filterValue = filterFns[ filterValue ] || filterValue;
   iso.arrange({ filter: filterValue });
 });
 
