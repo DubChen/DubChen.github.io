379395e205fec9e1beaa14ae6c0e581153270033
DubChen <wchen1642@gmail.com>
2016-09-27 00:09:40 -0700

    Button click debug

--- JS/ProjectFilter.js
+++ JS/ProjectFilter.js
@@ -14,6 +14,7 @@ filtersElem.addEventListener( 'click', function( event ) {
   if ( !matchesSelector( event.target, 'w3-btn' ) ) {
     return;
   }
+  alert("Button clicked!");
   var filterValue = event.target.getAttribute('data-filter');
   iso.arrange({ filter: filterValue });
 });
