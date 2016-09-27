a47506c71e474366e40f2788f13bd0e0125615c8
DubChen <wchen1642@gmail.com>
2016-09-27 10:23:57 -0700

    Change to jQuery
    Using jQuery format for Isotope filtering

--- JS/ProjectFilter.js
+++ JS/ProjectFilter.js
@@ -1,38 +1,23 @@
 // Script for filtering functionality on projects for home page
+// Uses jQuery and Isotope packages
 
 // init Isotope
-var iso = new Isotope( '.grid', {
+var $grid = $('.grid').isotope({
   itemSelector: '.element-item',
   layoutMode: 'fitRows'
 });
 
 
 // bind filter button click
-var filtersElem = document.querySelector('.filters-button-group');
-filtersElem.addEventListener( 'click', function( event ) {
-  // only work with buttons
-  if ( !matches( event.target, 'button' ) ) {
-      alert('Hi!');
-    return;
-  }
-  var filterValue = event.target.getAttribute('data-filter');
-  iso.arrange({ filter: filterValue });
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
-
 // change is-checked class on buttons
-var buttonGroups = document.querySelectorAll('.button-group');
-for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-  var buttonGroup = buttonGroups[i];
-  radioButtonGroup( buttonGroup );
-}
-
-function radioButtonGroup( buttonGroup ) {
-  buttonGroup.addEventListener( 'click', function( event ) {
-    // only work with buttons
-    if ( !matches( event.target, 'button' ) ) {
-      return;
-    }
-    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-    event.target.classList.add('is-checked');
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
   });
-}
+});
