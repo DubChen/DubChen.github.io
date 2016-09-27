479f2e7a81c94437b9b10ca6c306b8f207c6cdc2
DubChen <wchen1642@gmail.com>
2016-09-27 12:01:09 -0700

    Wrapping all Isotope functions inside image load

--- JS/ProjectFilter.js
+++ JS/ProjectFilter.js
@@ -6,18 +6,18 @@ var $grid = $('.grid').imagesLoaded( function() {
     itemSelector: '.element-item',
     layoutMode: 'fitRows'
   });
-});
 
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
   });
 });
